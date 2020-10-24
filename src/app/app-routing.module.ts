793cfa75097d56771c66c588d5cf19b2a4e9c173
JesusMrtzTorres <jesusmrtztorres@gmail.com>
2020-10-24 13:53:58 -0500

    Preload customers module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { SelectivePreloadingStrategyService } from './services/selective-preloading-strategy.service';
 
 const routes: Routes = [
   {
     path: 'customers',
-    loadChildren:  () => import('./customers/customers.module').then(m => m.CustomersModule)
+    loadChildren:  () => import('./customers/customers.module').then(m => m.CustomersModule),
+    data: {preload: true}
   },
   {
     path: 'orders',
@@ -20,7 +22,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     enableTracing: true,
-    preloadingStrategy: PreloadAllModules
+    preloadingStrategy: SelectivePreloadingStrategyService
   })],
   exports: [RouterModule]
 })
